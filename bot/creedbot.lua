879f53fc0052c71dcfea2233d938302dc2ea09ea
Shayan123456 <shayan_jon123@yahoo.com>
2016-02-12 10:19:07 +0330

    Update creedbot.lua

--- bot/creedbot.lua
+++ bot/creedbot.lua
@@ -209,6 +209,7 @@ function create_config( )
     "ingroup",
     "inpm",
     "banhammer",
+    "Boobs",
     ".",
     "Feedback",
     "plugins",
@@ -231,6 +232,7 @@ function create_config( )
     "sms",
     "anti_spam",
     "add_bot",
+    "time",
     "owners",
     "set",
     "get",
@@ -240,7 +242,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {172178919},--Sudo users
+    sudo_users = {87160007},--Sudo users
     disabled_chann144152859els = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -251,7 +253,7 @@ function create_config( )
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @acrazyboy10  〰〰〰〰〰〰〰〰
+   ߔࠀ   @shayan123hacker  〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
    !feedback (your ideas and messages)
